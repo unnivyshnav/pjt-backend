190caa895c9a8ef17b88f94e0247fdd0160d7680
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-02-22 17:41:02 +0530

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -4,6 +4,7 @@ const CourseSchema = new mongoose.Schema(
   {
     name: { type: String },
     description: { type: String },
+    fee: { type: Number },
     photo: { type: String, default: "" },
   },
   { timestamps: true }
