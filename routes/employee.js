811c7cab697f87539510655e48e30160d60912ef
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-03-13 09:19:30 +0530

    Update employee.js

--- routes/employee.js
+++ routes/employee.js
@@ -62,16 +62,16 @@ router.get("/find/:id", async (req, res) => {
 
 //test
 
-router.get("/", async (req, res) => {
-  try {
-    const employee = await Employee.find();
-    res.status(200).json(employee);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get("/", async (req, res) => {
+//   try {
+//     const employee = await Employee.find();
+//     res.status(200).json(employee);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// get approved students test
+// get approved employees test
 router.get("/", async (req, res) => {
   try {
     const employee = await Employee.find({ isApproved: true });
