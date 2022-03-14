c9ff5b160b397a079a3a877102918248c6819008
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-03-14 15:34:15 +0530

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -37,46 +37,30 @@ router.put("/:id", verify, async (req, res) => {
   }
 });
 
-// update test
-// router.put("/:id", async (req, res) => {
+// delete
+
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Course.findByIdAndDelete(req.params.id);
+      res.status(200).json("Course has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("Only admin can delete a course");
+  }
+});
+
+//delete test
+// router.delete("/:id", async (req, res) => {
 //   try {
-//     const updateCourse = await Course.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updateCourse);
+//     await Course.findByIdAndDelete(req.params.id);
+//     res.status(200).json("Course has been deleted");
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
 // });
-
-//delete original
-
-// router.delete("/:id", verify, async (req, res) => {
-//   if (req.user.isAdmin) {
-//     try {
-//       await Course.findByIdAndDelete(req.params.id);
-//       res.status(200).json("Course has been deleted");
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   } else {
-//     res.status(403).json("Only admin can delete a course");
-//   }
-// });
-
-//delete test
-router.delete("/:id", async (req, res) => {
-  try {
-    await Course.findByIdAndDelete(req.params.id);
-    res.status(200).json("Course has been deleted");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 //get
 router.get("/find/:id", async (req, res) => {
