1f4b94f3d52b74da900dec4c27c9e34944cf8cf6
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-03-16 15:13:38 +0530

    unique

--- models/Student.js
+++ models/Student.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const StudentSchema = new mongoose.Schema(
   {
     name: { type: String },
-    email: { type: String },
+    email: { type: String, unique:true },
     password: { type: String },
     phone: { type: Number },
     place: { type: String },
